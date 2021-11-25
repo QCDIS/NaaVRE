d057f3c6c69b33e423f998bce5bee09558680a02
Riccardo Bianchi <contervis@gmail.com>
2021-11-25 17:56:26 +0100

    issue-#23
    - Added cell deletion operation in experiment manager
    - Little modification to APIs structure to move towards REST compliance
    - Fixed import errors

--- packages/faircells-composer/src/components/SidebarItem.tsx
+++ packages/faircells-composer/src/components/SidebarItem.tsx
@@ -1,6 +1,8 @@
 import { INode, REACT_FLOW_CHART } from '@mrblenny/react-flow-chart'
+import { Dialog, showDialog } from '@jupyterlab/apputils';
 import * as React from 'react'
 import styled from 'styled-components'
+import { requestAPI } from '@jupyter_vre/core';
 
 const Outer = styled.div`
   padding: 15px;
@@ -19,12 +21,35 @@ export interface ISidebarItemProps {
 }
 
 export const SidebarItem = ({ type, ports, properties }: ISidebarItemProps) => {
+
+  function click() {
+    showDialog({
+      title: 'Delete',
+      body: (
+        <p>Confirm Deletion of '{properties['title']}' ?</p>
+      ),
+      buttons: [Dialog.okButton(), Dialog.cancelButton()]
+    }).then((res) => {
+
+        if (res.button.label == 'OK') {
+
+          requestAPI<any>('catalog/cells', {
+            body: JSON.stringify(properties),
+            method: 'DELETE'
+          }).then((resp) => {
+            console.log(resp);
+          });
+        }
+    });
+  }
+
   return (
     <Outer
+      onClick={click}
       draggable={true}
-      onDragStart={ (event: any) => {
+      onDragStart={(event: any) => {
         event.dataTransfer.setData(REACT_FLOW_CHART, JSON.stringify({ type, ports, properties }))
-      } }
+      }}
     >
       {properties['title']}
     </Outer>
