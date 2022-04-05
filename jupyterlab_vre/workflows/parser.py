2554e6e81cdb53787543f3acf55b7c5c7827f2ba
Riccardo Bianchi <contervis@gmail.com>
2022-04-05 23:47:26 +0200

    WIP: Merger and Splitter implementation as "synthetic" nodes inside the workflow templating

--- jupyterlab_vre/workflows/parser.py
+++ jupyterlab_vre/workflows/parser.py
@@ -22,58 +22,41 @@ class WorkflowParser:
 
         self.nodes = nodes
         self.links = links
-        self.splitters = defaultdict(dict)
-        self.dependencies = {nodes[node]['properties']['og_node_id']: [] for node in nodes if
-                             nodes[node]['type'] != 'splitter' and nodes[node]['type'] != 'merger'}
-        self.cells_in_use = {nodes[node]['properties']['og_node_id']: \
-                                 Catalog.get_cell_from_og_node_id(nodes[node]['properties']['og_node_id']) \
-                             for node in nodes if nodes[node]['type'] != 'splitter' and nodes[node]['type'] != 'merger'
-                             }
+
+        # TODO: Use og_node_id as index for non special nodes, id for the special ones
+
+        self.dependencies = {
+            nodes[node]['id']: [] for node in nodes
+        }
+
+        self.cells_in_use = {
+            nodes[node]['id']: Catalog.get_cell_from_og_node_id(nodes[node]['properties']['og_node_id'])
+            for node in nodes if nodes[node]['type'] != 'splitter' and nodes[node]['type'] != 'merger'
+        }
+
         self.__parse_links()
-        self.__resolve_splitters()
-
-    def __resolve_splitters(self):
-
-        for s, links in self.splitters.items():
-            og_id_to = self.__get_og_node_id(links['target']['to']['nodeId'])
-            og_id_from = self.__get_og_node_id(links['source']['from']['nodeId'])
-            from_cell = Catalog.get_cell_from_og_node_id(og_id_from)
-            self.dependencies[og_id_to].append({
-                'task_name': from_cell['task_name'],
-                'port_id': links['source']['from']['portId'],
-                'scaling': True
-            })
 
     def __parse_links(self):
 
-        '''
-            node1(pno) => (sps)splitter(spt) => (pni)node2
-            node1(pno) => (pni)node2
-        '''
-
         for k in self.links:
+
             link = self.links[k]
 
-            if link['to']['portId'] == 'splitter_source' or link['to']['portId'] == 'merger_source':
-                self.splitters[link['to']['nodeId']]['source'] = link
-                continue
+            to_node = self.nodes[link['to']['nodeId']]
+            from_node = self.nodes[link['from']['nodeId']]
 
-            if link['from']['portId'] == 'splitter_target' or link['to']['portId'] == 'merger_target':
-                self.splitters[link['from']['nodeId']]['target'] = link
+            from_special_node = (
+                from_node['type'] == 'merger' or from_node['type'] == 'splitter')
+            task_name = f'{from_node["type"]}_{from_node["id"]}_task' if from_special_node else Catalog.get_cell_from_og_node_id(
+                self.__get_og_node_id(from_node['id']))['task_name']
 
-            else:
-
-                og_id_to = self.__get_og_node_id(link['to']['nodeId'])
-                og_id_from = self.__get_og_node_id(link['from']['nodeId'])
-                from_cell = Catalog.get_cell_from_og_node_id(og_id_from)
-                self.dependencies[og_id_to].append({
-                    'task_name': from_cell['task_name'],
-                    'port_id': link['from']['portId'],
-                    'scaling': False
-                })
+            self.dependencies[to_node['id']].append({
+                'task_name': task_name,
+                'port_id': link['from']['portId'],
+                'type': from_node['type']
+            })
 
     def __get_og_node_id(self, node_id) -> str:
-        logger.debug('Node: ' + str(self.nodes[node_id]))
         return self.nodes[node_id]['properties']['og_node_id']
 
     def get_workflow_cells(self) -> dict:
