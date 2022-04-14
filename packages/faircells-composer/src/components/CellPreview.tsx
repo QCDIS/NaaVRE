edc071233dc3399360df64c7563880c9c717e3a3
Riccardo Bianchi <contervis@gmail.com>
2022-04-14 19:12:47 +0200

    Added catalog for dialog exploration with interactive preview and cell info

---
+++ packages/faircells-composer/src/components/CellPreview.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import { cloneDeep, mapValues } from 'lodash'
+import * as actions from "@mrblenny/react-flow-chart/src/container/actions";
+import { FlowChart, IChart } from "@mrblenny/react-flow-chart"
+import { NodeInnerCustom, PortCustom } from '@jupyter_vre/chart-customs';
+
+const defaultChart: IChart = {
+    offset: {
+        x: 0,
+        y: 0,
+    },
+    scale: 1,
+    nodes: {},
+    links: {},
+    selected: {},
+    hovered: {}
+}
+
+export class CellPreview extends React.Component {
+
+    public state = cloneDeep(defaultChart)
+
+    updateChart = (chart: IChart) => { this.setState(chart) }
+
+    public render() {
+
+        const chart = this.state
+        const stateActions = mapValues(actions, (func: any) =>
+            (...args: any) => this.setState(func(...args))) as typeof actions
+
+        return (
+            <div>
+                <FlowChart
+                    chart={chart}
+                    callbacks={stateActions}
+                    Components={{
+                        NodeInner: NodeInnerCustom,
+                        Port: PortCustom
+                    }}
+                />
+            </div>
+        )
+    }
+}
