62b4215da624990eb2752af0745564b544a559cd
skoulouzis <koulouzis@gmail.com>
2022-04-14 17:09:41 +0200

    activate env

--- docker/install_MULTIPLY.sh
+++ docker/install_MULTIPLY.sh
@@ -3,11 +3,9 @@
 pip install sentinelhub --upgrade
 
 mkdir dependencies
+cd dependencies
 
-git clone https://github.com/JorisTimmermans/Deploy_MULTIPLY.git
-cd Deploy_MULTIPLY
-conda env create -f environments/environment_multiply_platform.yml
-cd ../
+RUN source /venv/bin/activate
 
 git clone https://github.com/JorisTimmermans/atmospheric_correction.git
 cd atmospheric_correction
@@ -15,10 +13,39 @@ python setup.py develop
 cd ../
 
 
+git clone https://github.com/JorisTimmermans/BRDF_descriptors.git
+BRDF_descriptors
+python setup.py install
+cd ../
+
 git clone https://github.com/JorisTimmermans/data-access.git
 cd data-access
 python setup.py develop
 cd ../
 
 
+git clone https://github.com/multiply-org/inference-engine.git
+cd inference-engine
+python setup.py develop
+cd ../
+
+git clone https://github.com/JorisTimmermans/multiply-core.git
+cd multiply-core
+python setup.py develop
+cd ../
+
+git clone https://github.com/multiply-org/KaFKA-InferenceEngine.git
+cd KaFKA-InferenceEngine
+python setup.py develop
+cd ../
+
+git clone https://github.com/multiply-org/sar-pre-processing.git
+cd sar-pre-processing
+python setup.py develop
+cd ../
+
+git clone https://github.com/JorisTimmermans/vm-support.git
+cd vm-support
+cd python setup.py develop
+cd ../
 
