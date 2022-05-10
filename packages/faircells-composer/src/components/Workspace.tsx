9fdaa5fcc912edf15eb2d7bd6d3496f4fbca1589
Riccardo Bianchi <contervis@gmail.com>
2022-05-10 18:32:02 +0200

    Improved interaction between catalog dialog and workspace window

--- packages/faircells-composer/src/components/Workspace.tsx
+++ packages/faircells-composer/src/components/Workspace.tsx
@@ -20,26 +20,45 @@ export class Workspace extends React.Component {
         let currElements = this.state.workspace_elements;
         currElements.push(element);
         this.setState({ workspace_elements: currElements })
-        console.log(this.state);
+    }
+
+    removeElement = (index: number) => {
+
+        let currElements = this.state.workspace_elements;
+        currElements.splice(index, 1);
+        this.setState({ workspace_elements: currElements })
+    }
+
+    hasElement = (element: FairCell) => {
+        return this.state.workspace_elements.map((el) => {
+            return el.node_id
+        }).includes(element.node_id);
     }
 
     render() {
         return (
             <Box sx={{ background: 'white', height: 500, width: 250, transform: 'translateZ(0px)', flexGrow: 1, position: 'absolute', top: 20, left: 20 }}>
                 <p className='section-header'>Workspace</p>
-                <div className={'workspace-items-container'}>
-                    {this.state.workspace_elements.map((value, _index) => {
-                        let nodes = value['chart_obj']['nodes']
-                        let element = nodes[Object.keys(nodes)[0]]
-                        return (
-                            <WorkspaceItem
-                                type={element['type']}
-                                ports={element['ports']}
-                                properties={element['properties']}
-                            />
-                        )
-                    })}
-                </div>
+                {this.state.workspace_elements.length == 0 ? 
+                (<div className={'empty-workspace'}>The workspace is empty</div>) 
+                : (
+                    <div className={'workspace-items-container'}>
+                        {this.state.workspace_elements.map((value, index) => {
+                            let nodes = value['chart_obj']['nodes']
+                            let element = nodes[Object.keys(nodes)[0]]
+                            return (
+                                <WorkspaceItem
+                                    index={index}
+                                    type={element['type']}
+                                    ports={element['ports']}
+                                    properties={element['properties']}
+                                    itemDeleteAction={this.removeElement}
+                                />
+                            )
+                        })}
+                    </div>
+                )
+                }
             </Box>
         )
     }
