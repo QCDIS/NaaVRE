ae415103bfacba0f61518518788c5cc5fd8cecd1
skoulouzis <koulouzis@gmail.com>
2022-06-01 16:39:05 +0200

    only make reuse

--- make-relese.sh
+++ make-relese.sh
@@ -2,7 +2,7 @@
 
 conda activate jupyterlab
 
-make install-backend && make build-frontend && make install-ui && make link-ui
+#make install-backend && make build-frontend && make install-ui && make link-ui
 make release
 cp dist/jupyterlab_vre-0.1.0-py3-none-any.whl .
 
