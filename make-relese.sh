4d28675cfe0d0954106ac262e641b7c04f27cbf1
skoulouzis <koulouzis@gmail.com>
2022-06-01 16:31:44 +0200

    added manual release script

--- make-relese.sh
+++ make-relese.sh
@@ -4,7 +4,7 @@ conda activate jupyterlab
 
 make install-backend && make build-frontend && make install-ui && make link-ui
 make release
-cp ../dist/jupyterlab_vre-0.1.0-py3-none-any.whl .
+cp dist/jupyterlab_vre-0.1.0-py3-none-any.whl .
 
 sudo docker build . --file Dockerfile -t qcdis/n-a-a-vre
 sudo docker build . --file Dockerfile-laserfarm -t qcdis/n-a-a-vre-laserfarm:$1
