f48e28bdc4b43397c814f50658e2eccb71bdbe60
skoulouzis <koulouzis@gmail.com>
2022-06-01 22:25:12 +0200

    copy wheel in docker

--- make-relese.sh
+++ make-relese.sh
@@ -4,7 +4,7 @@ conda activate jupyterlab
 
 #make install-backend && make build-frontend && make install-ui && make link-ui
 make release
-cp dist/jupyterlab_vre-0.1.0-py3-none-any.whl .
+cp dist/jupyterlab_vre-0.1.0-py3-none-any.whl docker/
 
 cd docker
 sudo docker build . --file Dockerfile -t qcdis/n-a-a-vre
