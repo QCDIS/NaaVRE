c90ef4919bcde9701be2199b23f873455095ca64
skoulouzis <koulouzis@gmail.com>
2022-06-02 09:52:52 +0200

    tag latest

--- make-relese.sh
+++ make-relese.sh
@@ -13,17 +13,25 @@ sudo docker build . --file Dockerfile-vol2bird -t qcdis/n-a-a-vre-vol2bird:$1
 sudo docker build . --file Dockerfile-MULTIPLY -t qcdis/n-a-a-vre-multiply:$1
 sudo docker build . --file Dockerfile-pytorch -t qcdis/n-a-a-vre-pytorch:$1
 
-sudo docker push qcdis/n-a-a-vre:$1
-sudo docker push qcdis/n-a-a-vre-laserfarm:$1
-sudo docker push qcdis/n-a-a-vre-vol2bird:$1
-sudo docker push qcdis/n-a-a-vre-multiply:$1
-sudo docker push qcdis/n-a-a-vre-pytorch:$1
+sudo docker push qcdis/n-a-a-vre:"$1"
+sudo docker push qcdis/n-a-a-vre-laserfarm:"$1"
+sudo docker push qcdis/n-a-a-vre-vol2bird:"$1"
+sudo docker push qcdis/n-a-a-vre-multiply:"$1"
+sudo docker push qcdis/n-a-a-vre-pytorch:"$1"
 
-
+sudo docker tag qcdis/n-a-a-vre:"$1" qcdis/n-a-a-vre:latest
 sudo docker push qcdis/n-a-a-vre:latest
+
+sudo docker tag qcdis/n-a-a-vre-laserfarm:"$1" qcdis/n-a-a-vre:latest
 sudo docker push qcdis/n-a-a-vre-laserfarm:latest
+
+sudo docker tag qcdis/n-a-a-vre-vol2bird:"$1" qcdis/n-a-a-vre-vol2bird:latest
 sudo docker push qcdis/n-a-a-vre-vol2bird:latest
+
+sudo docker tag qcdis/n-a-a-vre-multiply:"$1" qcdis/n-a-a-vre-multiply:latest
 sudo docker push qcdis/n-a-a-vre-multiply:latest
+
+sudo docker tag qcdis/n-a-a-vre-pytorch:"$1" qcdis/n-a-a-vre-pytorch:latest
 sudo docker push qcdis/n-a-a-vre-pytorch:latest
 
 cd ../
