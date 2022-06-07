a524f372e0ffaa0d86978dcb08c59d7d7c027374
skoulouzis <koulouzis@gmail.com>
2022-06-07 23:02:06 +0200

    remove github3

--- make-relese.sh
+++ make-relese.sh
@@ -22,7 +22,7 @@ sudo docker push qcdis/n-a-a-vre-pytorch:"$1"
 sudo docker tag qcdis/n-a-a-vre:"$1" qcdis/n-a-a-vre:latest
 sudo docker push qcdis/n-a-a-vre:latest
 
-sudo docker tag qcdis/n-a-a-vre-laserfarm:"$1" qcdis/n-a-a-vre:latest
+sudo docker tag qcdis/n-a-a-vre-laserfarm:"$1" qcdis/n-a-a-vre-laserfarm:latest
 sudo docker push qcdis/n-a-a-vre-laserfarm:latest
 
 sudo docker tag qcdis/n-a-a-vre-vol2bird:"$1" qcdis/n-a-a-vre-vol2bird:latest
