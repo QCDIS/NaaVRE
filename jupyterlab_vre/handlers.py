223716727ca11f31de8193d5beb74f4e57ec6e26
skoulouzis <koulouzis@gmail.com>
2022-06-12 15:50:34 +0200

    check if JUPYTERHUB_USER is in os.environ

--- jupyterlab_vre/handlers.py
+++ jupyterlab_vre/handlers.py
@@ -36,7 +36,8 @@ module_mapping = {
     "cv2": "opencv-python-headless"
 }
 
-current_username = os.environ['JUPYTERHUB_USER']
+if 'JUPYTERHUB_USER' in os.environ:
+    current_username = os.environ['JUPYTERHUB_USER']
 
 
 # code from https://stackoverflow.com/questions/552659/how-to-assign-a-git-sha1s-to-a-file-without-git
