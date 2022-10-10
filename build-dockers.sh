35aa63155c42f3f4d10298e5de645eb94d492292
skoulouzis <koulouzis@gmail.com>
2022-10-10 14:42:27 +0200

    marge laserfarm and pycharm

--- build-dockers.sh
+++ build-dockers.sh
@@ -6,7 +6,7 @@ cd docker
 sudo docker build . --file Dockerfile-laserfarm -t qcdis/n-a-a-vre-laserfarm:$1
 sudo docker build . --file Dockerfile-vol2bird -t qcdis/n-a-a-vre-vol2bird:$1
 sudo docker build . --file Dockerfile-MULTIPLY -t qcdis/n-a-a-vre-multiply:$1
-sudo docker build . --file Dockerfile-pytorch -t qcdis/n-a-a-vre-pytorch:$1
+sudo docker build . --file Dockerfile-laserfarm -t qcdis/n-a-a-vre-pytorch:$1
 
 sudo docker push qcdis/n-a-a-vre-laserfarm:"$1"
 sudo docker push qcdis/n-a-a-vre-vol2bird:"$1"
