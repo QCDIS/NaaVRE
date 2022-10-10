2e4eb993ec5c6d701e7e3d3081f61d21457861e5
skoulouzis <koulouzis@gmail.com>
2022-10-10 14:44:10 +0200

    marge laserfarm and pycharm

--- build-dockers.sh
+++ build-dockers.sh
@@ -3,10 +3,10 @@
 cp dist/jupyterlab_vre-0.1.0-py3-none-any.whl docker/
 cd docker
 
-sudo docker build . --file Dockerfile-laserfarm -t qcdis/n-a-a-vre-laserfarm:$1
+sudo docker build . --file Dockerfile-laserfarm-pytorch -t qcdis/n-a-a-vre-laserfarm:$1
 sudo docker build . --file Dockerfile-vol2bird -t qcdis/n-a-a-vre-vol2bird:$1
 sudo docker build . --file Dockerfile-MULTIPLY -t qcdis/n-a-a-vre-multiply:$1
-sudo docker build . --file Dockerfile-laserfarm -t qcdis/n-a-a-vre-pytorch:$1
+sudo docker build . --file Dockerfile-laserfarm-pytorch -t qcdis/n-a-a-vre-pytorch:$1
 
 sudo docker push qcdis/n-a-a-vre-laserfarm:"$1"
 sudo docker push qcdis/n-a-a-vre-vol2bird:"$1"
