cf578fe8b8c592cc4817fa8cc278242315c11740
skoulouzis <koulouzis@gmail.com>
2022-10-13 19:04:04 +0200

    replace hardcoded values with env vars

--- jupyterlab_vre/notebook_search/handlers.py
+++ jupyterlab_vre/notebook_search/handlers.py
@@ -22,9 +22,15 @@ class NotebookSearchHandler(APIHandler):
     @web.authenticated
     async def post(self, *args, **kwargs):
         payload = self.get_json_body()
+        logger.info('payload: ' + str(payload))
         term = payload['keyword']
         search_api_endpoint = os.getenv('SEARCH_API_ENDPOINT')
         search_api_token = os.getenv('SEARCH_API_TOKEN')
+        if not search_api_endpoint:
+            search_api_endpoint = 'http://145.100.135.119/api/notebook_search/'
+        if not search_api_token:
+            search_api_token = 'b30bd18ea01f5a45e217b03682f70ce6ae14c293'
+        logger.info('search_api_endpoint: ' + str(search_api_endpoint))
         response = requests.get(
             search_api_endpoint,
             params={
@@ -37,9 +43,10 @@ class NotebookSearchHandler(APIHandler):
             headers={
                 "Accept": "*/*",
                 "Content-Type": "text/json",
-                "Authorization": "Token "+search_api_token
+                "Authorization": "Token " + str(search_api_token)
             }
         )
         hits = response.text
+        logger.info('hits: ' + str(hits))
         self.write(hits)
         self.flush()
