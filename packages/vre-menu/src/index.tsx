0417dc38dd690ef51c717d17fe48de012079aa1a
skoulouzis <koulouzis@gmail.com>
2023-03-11 21:14:58 +0100

    import composer to menue

--- packages/vre-menu/src/index.tsx
+++ packages/vre-menu/src/index.tsx
@@ -1,43 +1,75 @@
+import {ComposerWidget} from "@jupyter_vre/experiment-manager/lib/Composer"
+
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin,
+  JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { IMainMenu } from '@jupyterlab/mainmenu';
+import { Menu } from '@lumino/widgets';
 import { ICommandPalette } from '@jupyterlab/apputils';
 
-/**
- * Initialization data for the main menu example.
- */
+import { MainAreaWidget } from '@jupyterlab/apputils';
+
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'main-menu',
   autoStart: true,
-  requires: [ICommandPalette],
-  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
-    const { commands } = app;
+  requires: [ICommandPalette, IMainMenu],
+  activate: (
+      app: JupyterFrontEnd,
+      palette: ICommandPalette,
+      mainMenu: IMainMenu
+  ) => {
 
-    // Add a command
-    const command = 'jlab-examples:main-menu';
-    commands.addCommand(command, {
-      label: 'NaaVSettingsRE',
-      caption: 'Settings',
-      execute: (args: any) => {
-        console.log(
-          `jlab-examples:main-menu has been called ${args['origin']}.`
-        );
-        window.alert(
-          `jlab-examples:main-menu has been called ${args['origin']}.`
-        );
-      },
-    });
+      const { commands } = app;
+      const commandSettings = 'naavre:settings';
 
-    // Add the command to the command palette
-    const category = 'NaaVRE';
-    palette.addItem({
-      command,
-      category,
-      args: { origin: 'from the palette' },
-    });
-  },
+      commands.addCommand(commandSettings, {
+          label: 'Settings',
+          caption: 'Settings',
+          execute: (args: any) => {
+              
+              // TODO: Open dedicated settings page
+          }
+      });
+
+
+      const commandExperimentManager = 'naavre:experiment-manager';
+
+      commands.addCommand(commandExperimentManager, {
+          label: 'CounterWidget',
+          caption: 'CounterWidget',
+          execute: (args: any) => {
+              const content = new ComposerWidget();
+              const widget = new MainAreaWidget<ComposerWidget>({ content });
+              widget.title.label = 'CounterWidget';
+              app.shell.add(widget, 'main');
+          }
+      });
+
+      const category = 'NaaVRE';
+
+      palette.addItem({
+          command: commandSettings,
+          category,
+          args: { origin: 'from the palette' }
+      });
+
+      palette.addItem({
+          command: commandExperimentManager,
+          category,
+          args: { origin: 'from the palette' }
+      });
+
+      // Create a menu
+      const vreMenu: Menu = new Menu({ commands });
+      vreMenu.title.label = 'NaaVRE'
+      mainMenu.addMenu(vreMenu, { rank: 80 });
+      vreMenu.addItem({ command: commandSettings, args: { origin: 'from the menu' } });
+      vreMenu.addItem({ command: commandExperimentManager, args: { origin: 'from the menu' } });
+
+  }
 };
 
 export default extension;
+
