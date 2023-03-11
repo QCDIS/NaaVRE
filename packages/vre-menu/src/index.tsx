d86173b6b39923be5198e0e5ae223bd0249b239e
skoulouzis <koulouzis@gmail.com>
2023-03-11 15:53:37 +0100

    use schema

--- packages/vre-menu/src/index.tsx
+++ packages/vre-menu/src/index.tsx
@@ -1,43 +1,43 @@
 import {
-    JupyterFrontEnd,
-    JupyterFrontEndPlugin,
-  } from '@jupyterlab/application';
-  
-  import { ICommandPalette } from '@jupyterlab/apputils';
-  
-  /**
-   * Initialization data for the main menu example.
-   */
-  const extension: JupyterFrontEndPlugin<void> = {
-    id: 'main-menu',
-    autoStart: true,
-    requires: [ICommandPalette],
-    activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
-      const { commands } = app;
-  
-      // Add a command
-      const command = 'jlab-examples:main-menu';
-      commands.addCommand(command, {
-        label: 'Execute jlab-examples:main-menu Command',
-        caption: 'Execute jlab-examples:main-menu Command',
-        execute: (args: any) => {
-          console.log(
-            `jlab-examples:main-menu has been called ${args['origin']}.`
-          );
-          window.alert(
-            `jlab-examples:main-menu has been called ${args['origin']}.`
-          );
-        },
-      });
-  
-      // Add the command to the command palette
-      const category = 'Extension Examples';
-      palette.addItem({
-        command,
-        category,
-        args: { origin: 'from the palette' },
-      });
-    },
-  };
-  
-  export default extension;
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin,
+} from '@jupyterlab/application';
+
+import { ICommandPalette } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the main menu example.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'main-menu',
+  autoStart: true,
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    const { commands } = app;
+
+    // Add a command
+    const command = 'jlab-examples:main-menu';
+    commands.addCommand(command, {
+      label: 'NaaVSettingsRE',
+      caption: 'Settings',
+      execute: (args: any) => {
+        console.log(
+          `jlab-examples:main-menu has been called ${args['origin']}.`
+        );
+        window.alert(
+          `jlab-examples:main-menu has been called ${args['origin']}.`
+        );
+      },
+    });
+
+    // Add the command to the command palette
+    const category = 'NaaVRE';
+    palette.addItem({
+      command,
+      category,
+      args: { origin: 'from the palette' },
+    });
+  },
+};
+
+export default extension;
