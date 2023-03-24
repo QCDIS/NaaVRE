04ec23fe5c7e3054a5ac6c311f4795bbbea8d494
skoulouzis <koulouzis@gmail.com>
2023-03-24 17:04:07 +0100

    move all init commands to script

--- docker/init_script.sh
+++ docker/init_script.sh
@@ -4,6 +4,14 @@ mkdir -p /home/jovyan/NaaVRE
 cp -r /tmp/repo_utils/conf_vl_repos.py /home/jovyan/NaaVRE/
 gitpuller https://github.com/QCDIS/lifewatch-notebooks main example_notebooks
 rm -f -- /home/jovyan/NaaVRE/module_name_mapping.json
-rm -rf ~/.conda/environments.txt
-wget https://github.com/QCDIS/data-access/raw/master/multiply.zip -O /tmp
-unzip multiply.zip
+
+DIR=/home/jovyan/.multiply/
+if [ -d "$DIR" ];
+then
+    echo "$DIR exists. skipping"
+else
+  wget $MULTIPLY_CONF_URL -O /tmp/multiply.zip
+  unzip /tmp/multiply.zip -d ~/.multiply
+fi
+
+python /tmp/repo_utils/conf_vl_repos.py --force=True --github_url=$CELL_GITHUB --github_token=$CELL_GITHUB_TOKEN --registry_url=$REGISTRY_URL
