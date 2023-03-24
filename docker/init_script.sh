b82f602f950ebe0c9fb621b744043ae7514ca735
skoulouzis <koulouzis@gmail.com>
2023-03-24 14:23:03 +0100

    COPY init_script.sh .

---
+++ docker/init_script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p /home/jovyan/NaaVRE
+cp -r /tmp/repo_utils/conf_vl_repos.py /home/jovyan/NaaVRE/
+gitpuller https://github.com/QCDIS/lifewatch-notebooks main example_notebooks
+rm -f -- /home/jovyan/NaaVRE/module_name_mapping.json
+rm -rf ~/.conda/environments.txt
+wget https://github.com/QCDIS/data-access/raw/master/multiply.zi
+unzip multiply.zip
