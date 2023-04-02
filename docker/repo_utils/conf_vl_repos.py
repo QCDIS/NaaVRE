a5ea57b3cedb8d83ea5d1fb0b3d3522d5b68eef9
skoulouzis <koulouzis@gmail.com>
2023-04-02 18:42:41 +0200

    conf script without dependencies and with environment variables

--- docker/repo_utils/conf_vl_repos.py
+++ docker/repo_utils/conf_vl_repos.py
@@ -1,67 +1,65 @@
 import argparse
 import logging
+import os
+from pathlib import Path
 
-from jupyterlab_vre.database.database import Catalog
-from jupyterlab_vre.repositories.repository import Repository
+from tinydb import TinyDB, where
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 arg_parser = argparse.ArgumentParser()
 
+naa_vre_path = os.path.join(str(Path.home()), 'NaaVRE')
+
+if not os.path.exists(naa_vre_path):
+    os.mkdir(naa_vre_path)
+
+db_path = os.path.join(naa_vre_path, 'NaaVRE_db.json')
+db = TinyDB(db_path)
+
+cells = db.table('cells')
+workflows = db.table('workflows')
+repositories = db.table('repositories')
+gh_credentials = db.table('gh_credentials')
+registry_credentials = db.table('registry_credentials')
+search_entry = db.table('search_entries')
+
 arg_parser.add_argument('--force', action='store', type=bool, required='True', dest='force')
-arg_parser.add_argument('--github_url', action='store', type=str, required='True', dest='github_url')
-arg_parser.add_argument('--github_token', action='store', type=str, required='True', dest='github_token')
-
-arg_parser.add_argument('--registry_url', action='store', type=str, required='True', dest='registry_url')
-# arg_parser.add_argument('--registry_token', action='store', type=str, required='True', dest='registry_token')
-
 args = arg_parser.parse_args()
 
 force = args.force
-github_url = args.github_url
-github_token = args.github_token
-
-registry_url = args.registry_url
-
-
-# registry_token = args.registry_token
 
 
 def add_gh_credentials(force_replace=None, repository_credentials=None):
     if force_replace:
-        Catalog.delete_all_gh_credentials()
-        Catalog.add_gh_credentials(repository_credentials)
-    else:
-        gh_credentials = Catalog.get_gh_credentials()
-        if not gh_credentials:
-            Catalog.add_gh_credentials(repository_credentials)
+        gh_credentials.remove(where('url') == repository_credentials['url'])
+    gh_credentials.insert(repository_credentials)
 
 
-def add_registry_credentials(force_replace, registry_credentials):
+def add_registry_credentials(force_replace, input_registry_credentials):
     if force_replace:
-        Catalog.delete_all_registry_credentials()
-        Catalog.add_registry_credentials(registry_credentials)
-    else:
-        registry_credentials = Catalog.get_gh_credentials()
-        if not registry_credentials:
-            Catalog.add_registry_credentials(registry_credentials)
+        registry_credentials.remove(where('url') == input_registry_credentials['url'])
+    registry_credentials.insert(input_registry_credentials)
 
 
 def add_repository_credentials(force_replace, repository_credentials):
     if force_replace:
-        Catalog.delete_all_repository_credentials()
-        Catalog.add_repository_credentials(repository_credentials)
-    else:
-        gh_credentials = Catalog.get_repository_credentials()
-        if not gh_credentials:
-            Catalog.add_repository_credentials(repository_credentials)
+        repositories.remove(where('url') == repository_credentials['url'])
+    repositories.insert(repository_credentials)
 
 
 if __name__ == '__main__':
-    input_repository_credentials = Repository(github_url.split('https://github.com/')[1], github_url, github_token)
+    github_url = os.getenv('CELL_GITHUB')
+    github_token = os.getenv('CELL_GITHUB_TOKEN')
+    registry_url = os.getenv('REGISTRY_URL')
+
+    print(github_url)
+    input_repository_credentials = {'name': github_url.split('https://github.com/')[1], 'url': github_url,
+                                    'token': github_token}
     add_gh_credentials(force_replace=force, repository_credentials=input_repository_credentials)
     add_repository_credentials(force_replace=force, repository_credentials=input_repository_credentials)
 
-    input_registry_credentials = Repository(registry_url.split('https://hub.docker.com/')[1], registry_url, None)
-    add_registry_credentials(force_replace=force, registry_credentials=input_registry_credentials)
+    input_registry_credentials = {'name': registry_url.split('https://hub.docker.com/')[1], 'url': registry_url,
+                                  'token': None}
+    add_registry_credentials(force_replace=force, input_registry_credentials=input_registry_credentials)
