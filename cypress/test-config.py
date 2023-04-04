f6bb68c618fa3b19ab8b67c4b96ab49f8146fe47
skoulouzis <koulouzis@gmail.com>
2023-04-04 19:23:33 +0200

    remove test-config.py to go through tests

--- cypress/test-config.py
+++
@@ -1,8 +0,0 @@
-
-c.Session.debug = True
-c.ServerApp.quit_button = False
-c.ServerApp.port = 8888
-c.ServerApp.port_retries = 0
-c.LabApp.open_browser = False
-c.ServerApp.root_dir = "./build/cypress-cypress"
-c.ServerApp.token = "TOKEN"
