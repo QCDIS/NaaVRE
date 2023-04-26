c4ffd2b834303e3910565ed8599b7d376c5bac6e
skoulouzis <koulouzis@gmail.com>
2023-04-26 16:28:34 +0200

    added laserfarm tests

--- docker/laserfarm-pytorch/test_laserchicken.py
+++ docker/laserfarm-pytorch/test_laserchicken.py
@@ -1,2 +1,11 @@
 from laserchicken.utils import create_point_cloud, add_to_point_cloud, get_point
 create_point_cloud([], [], [])
+
+
+import fnmatch
+from dask.distributed import LocalCluster, SSHCluster 
+from laserfarm import Retiler, DataProcessing, GeotiffWriter, MacroPipeline
+from laserfarm.remote_utils import get_wdclient, get_info_remote, list_remote
+from laserfarm.data_processing import DataProcessing
+
+
