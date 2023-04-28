0216a3a34a87356b91cf0558dbe4ebca729b1e54
skoulouzis <koulouzis@gmail.com>
2023-04-28 17:23:01 +0200

    added test

--- cypress/e2e/launcher.cy.ts
+++ cypress/e2e/launcher.cy.ts
@@ -1,15 +1,6 @@
-describe('Experiment Manager', () => {
-  beforeEach(() => {
-    cy.resetJupyterLab();
-  });
+describe('Test page', () => {
 
-  it('should have Experiment Manager extensions', () => {
-    // cy.get('.jp-ToolbarButtonComponent[title="New Launcher"]').click({
-    //   force: true
-    // });
-    // Jupyter notebook default kernel is available
-    cy.get(
-      '.jp-LauncherCard[data-category="Notebook"][title*="Python 3"]:visible'
-    );
-  });
-});
+  it('Reachability', () => {
+    cy.visit('/')
+  })
+})
