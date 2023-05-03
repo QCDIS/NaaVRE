aaba5a6e0d05e8e9f12bdad316bcf944732fee4d
Diederik Blanson Henkemans <diederikblansonhenkemans@Codact.local>
2023-05-03 11:46:32 +0200

    redirect output to json file

--- jupyterlab_vre/services/containerizer/Rcontainerizer.py
+++ jupyterlab_vre/services/containerizer/Rcontainerizer.py
@@ -1,5 +1,7 @@
 import os
 
+# TODO: create an interface for other programming languages
+
 class Rcontainerizer:
 
     @staticmethod
@@ -41,31 +43,34 @@ class Rcontainerizer:
         with open(files_info['cell']['path'], "w") as file:
             file.write("setwd('/app') \n\n")
 
-            # in case there are input types, we should dynamically add this value to the script
+            # we also want to always add the id to the input parameters
+            inputs = cell.types 
+            inputs['id'] = 'str'
+
+            # we should dynamically add this value to the script
             # this is done using the 'optparse' library, which does something similar as in the Python script
-            if len(cell.types) > 0:
-                file.write("# retrieve input parameters")
-                file.write("library(optparse) \n")
-                file.write("option_list = list( \n")
+            file.write("# retrieve input parameters\n")
+            file.write("library(optparse) \n")
+            file.write("option_list = list( \n")
 
-                for i, (key, value) in enumerate(cell.types.items()): # TODO: support more types and write more clean
-                    type = None
-                    if value == "str":
-                        type = "character"
-                    elif value == "int":
-                        type = "integer"
-                    else:
-                        raise ValueError("Not a valid type")
+            for i, (key, value) in enumerate(inputs.items()): # TODO: support more types and write more clean
+                type = None
+                if value == "str":
+                    type = "character"
+                elif value == "int":
+                    type = "integer"
+                else:
+                    raise ValueError("Not a valid type")
 
-                    file.write('''\t make_option(c("--{}"), action="store", default=NA, type='{}', help="my description")'''.format(key, type)) # https://gist.github.com/ericminikel/8428297
-                    
-                    if i != len(cell.types) - 1:
-                        file.write(",")
-                    file.write("\n")
+                file.write('''\t make_option(c("--{}"), action="store", default=NA, type='{}', help="my description")'''.format(key, type)) # https://gist.github.com/ericminikel/8428297
+                
+                if i != len(cell.types) - 1:
+                    file.write(",")
+                file.write("\n")
 
-                file.write(")\n\n")
-                file.write("# set input parameters accordingly \n")
-                file.write("opt = parse_args(OptionParser(option_list=option_list)) \n")
+            file.write(")\n\n")
+            file.write("# set input parameters accordingly \n")
+            file.write("opt = parse_args(OptionParser(option_list=option_list)) \n")
 
             # replace inputs
             original_source = cell.original_source
@@ -76,6 +81,16 @@ class Rcontainerizer:
             # print source
             file.write("# source code \n")
             file.write(original_source)
+
+            # outputs
+            outputs = ['myOutputVar'] # TODO: retrieve this dynamically
+
+            if len(outputs) > 0:
+                file.write("\n\n# capturing outputs \n")
+                for out in outputs:
+                    file.write("file <- file(paste0('/tmp/{}_', id, '.json')) \n".format(out))
+                    file.write("writeLines(as.character({}), file) \n".format(out))
+                    file.write("close(file) \n")
 
         # create the Dockerfile
         with open(files_info['dockerfile']['path'], "w") as file:
