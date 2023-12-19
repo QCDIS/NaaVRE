6dd0d4b3f6b34a9cd390e688550189de60e2a0e5
Gabriel Pelouze <gabriel@pelouze.net>
2023-12-19 15:46:02 +0100

    populate cell popup in experiment manager

--- packages/components/src/CellEditor.tsx
+++ packages/components/src/CellEditor.tsx
@@ -1,25 +1,50 @@
 import Box from '@mui/material/Box';
 import * as React from 'react';
-import { VRECell } from '@jupyter_vre/core';
+import {VRECell} from '@jupyter_vre/core';
+import {CellInfo} from "./CellInfo";
 
 interface CellEditorProps {
-    cell: VRECell
+  cell: VRECell
 }
 
 export class CellEditor extends React.Component<CellEditorProps> {
 
-    constructor(props: CellEditorProps) {
-        super(props);
-    }
+  cellInfoRef: React.RefObject<CellInfo>;
 
-    render() {
-        return (
-            <Box sx={{ borderRadius: '15px', boxShadow: '1px 1px lightgrey', background: 'white', height: 500, width: 500, transform: 'translateZ(0px)', flexGrow: 1, position: 'absolute', top: 20, right: 20 }}>
-                <p className='cell-editor section-header'>{this.props.cell.title}</p>
-                <div >
-                    
-                </div>
-            </Box>
-        )
-    }
+  constructor(props: CellEditorProps) {
+    super(props);
+    this.cellInfoRef = React.createRef()
+  }
+
+  componentDidMount() {
+    this.cellInfoRef.current.updateCell(
+      this.props.cell.chart_obj.nodes[this.props.cell.node_id],
+      this.props.cell.types as unknown as [],
+    )
+  }
+
+  render() {
+    return (
+      <Box sx={{
+        borderRadius: '15px',
+        border: 1,
+        borderColor: 'lightgrey',
+        boxShadow: '1px 1px lightgrey',
+        background: 'white',
+        zIndex: 10,
+        height: 500,
+        width: 500,
+        transform: 'translateZ(0px)',
+        flexGrow: 1,
+        position: 'absolute',
+        top: 20,
+        right: 20
+      }}>
+        <p className='cell-editor section-header'>{this.props.cell.title}</p>
+        <div>
+          <CellInfo ref={this.cellInfoRef}/>
+        </div>
+      </Box>
+    )
+  }
 }
