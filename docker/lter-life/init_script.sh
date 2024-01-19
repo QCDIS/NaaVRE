ae379f033c2097de7e981afae19ef3730a40e232
Gabriel Pelouze <gabriel@pelouze.net>
2024-01-19 11:52:52 +0100

    remvove build files introduced by commit bff7a89
    https://github.com/QCDIS/NaaVRE/commit/bff7a89d31cc423d143fb03f4822cc49b7fe1df9

--- docker/lter-life/init_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p $HOME/NaaVRE/
-mkdir -p /tmp/data
-cp -r /tmp/repo_utils/conf_vl_repos.py $HOME/NaaVRE/
-rm -f -- $HOME/NaaVRE/module_name_mapping.json
-python /tmp/repo_utils/conf_vl_repos.py --force=False
