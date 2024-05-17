4a6c1d0061799309d4e4367ed339a2a19b7f7c45
Jordi Boon <jordi.boon01@gmail.com>
2024-05-17 15:36:59 +0200

    Visitor for extracting all variables in text

--- jupyterlab_vre/services/extractor/parseR/Visitors.py
+++ jupyterlab_vre/services/extractor/parseR/Visitors.py
@@ -2,8 +2,159 @@ from .RVisitor import RVisitor
 from .RParser import RParser
 import re
 
+class ExtractNames(RVisitor):
+    # Get all names and try to infer their types.
+    def __init__(self):
+        self.names = {}
+        self.scoped = set()
 
-class ExtractDefs(RVisitor):
+    def visitProg(self, ctx: RParser.ProgContext):
+        self.visitChildren(ctx)
+        return self.names
+
+    def visitCall(self, ctx: RParser.CallContext):
+        if isinstance(ctx.expr(), RParser.AssignContext):
+            self.visit(ctx.expr())
+
+        self.visit(ctx.sublist())
+
+        return None
+
+    def visitFor(self, ctx: RParser.ForContext):
+        # Iterator variable is scoped
+        self.scoped.add(ctx.ID().getText())
+        # If what we iterate over is a variable, type should be list.
+        if isinstance(ctx.expr(0), RParser.IdContext):
+            self.visit(ctx.expr(0))
+            id = ctx.expr(0).getText()
+            self.names[id]['type'] = 'list'
+        self.visit(ctx.expr(1))
+        self.scoped.remove(ctx.ID().getText())
+        return None
+
+    def visitSub(self, ctx: RParser.SubContext):
+        if isinstance(ctx.expr(), RParser.IdContext):
+            self.visit(ctx.expr())
+        return None
+
+    def visitSublist(self, ctx: RParser.SublistContext):
+        self.visitChildren(ctx)
+        return None
+
+    def visitAssign(self, ctx: RParser.AssignContext):
+        # Get the identifier and the assigned value of the expr and add to dict.
+        id = self.visit(ctx.expr(0))
+
+        if id is None:
+            return None
+
+        if id in self.names and self.names[id]['type'] is not None:
+            return None
+
+        # Check if the value is an ID or an expression of which we can get type.
+        if isinstance(ctx.expr(1), RParser.IdContext):
+            self.names[id] = {'name': id, 'type': None}
+        else:
+            type = self.visit(ctx.expr(1))
+            self.names[id] = {'name': id, 'type': type}
+
+    #     return None
+
+    def visitAddsub(self, ctx: RParser.AddsubContext):
+        # Visit left and right expressions
+        left = self.visit(ctx.expr(0))
+        right = self.visit(ctx.expr(1))
+
+        # Check if left and right are in dict i.e. they're variables
+        if left in self.names:
+            left = self.names[left]
+        if right in self.names:
+            right = self.names[right]
+
+        # Check if the left and right children are of the same type
+        if (left == "int" and right == "int"):
+            return "int"
+        elif (left == "float" or right == "float"):
+            return "float"
+        else:
+            return None
+
+    def visitMuldiv(self, ctx: RParser.MuldivContext):
+        # Same procedure as addsub
+        left = self.visit(ctx.expr(0))
+        right = self.visit(ctx.expr(1))
+
+        if left in self.names:
+            left = self.names[left]
+        if right in self.names:
+            right = self.names[right]
+
+        if (left == "int" and right == "int"):
+            return "int"
+        elif (left == "float" or right == "float"):
+            return "float"
+        else:
+            return None
+
+    def visitId(self, ctx: RParser.IdContext):
+        id = ctx.ID().getText()
+        # Check if id is in dict, otherwise add it.
+        if id not in self.names and id not in self.scoped:
+            self.names[id] = {'name': id, 'type': None}
+        return id
+
+    def visitInt(self, ctx: RParser.IntContext):
+        return "int"
+
+    def visitFloat(self, ctx: RParser.FloatContext):
+        return "float"
+
+    def visitString(self, ctx: RParser.StringContext):
+        return "str"
+
+
+class ExtractUndefined(RVisitor):
+    def __init__(self, defs):
+        self.undefined = set()
+        self.defs = defs
+        self.scoped = set()
+
+    def visitProg(self, ctx: RParser.ProgContext):
+        self.visitChildren(ctx)
+        return self.undefined
+
+    def visitAssign(self, ctx: RParser.AssignContext):
+        return None
+
+    def visitCall(self, ctx: RParser.CallContext):
+        self.visit(ctx.sublist())
+        return None
+
+    def visitSublist(self, ctx: RParser.SublistContext):
+        self.visitChildren(ctx)
+        return None
+
+    def visitSub(self, ctx: RParser.SubContext):
+        if isinstance(ctx.expr(), RParser.IdContext):
+            self.visit(ctx.expr())
+        return None
+
+    def visitFor(self, ctx: RParser.ForContext):
+        # Iterator variable is scoped
+        self.scoped.add(ctx.ID().getText())
+        self.visit(ctx.expr(0))
+        self.visit(ctx.expr(1))
+        self.scoped.remove(ctx.ID().getText())
+        return None
+
+    def visitId(self, ctx: RParser.IdContext):
+        id = ctx.ID().getText()
+        if id not in self.defs and id not in self.scoped:
+            self.undefined.add(ctx.getText())
+        return None
+
+
+class ExtractDefined(RVisitor):
     def __init__(self):
         self.defs = set()
 
@@ -28,67 +179,6 @@ class ExtractDefs(RVisitor):
 
     def visitId(self, ctx: RParser.IdContext):
         return ctx.getText()
-
-
-class ExtractInputs(RVisitor):
-    def __init__(self, defs):
-        self.defs = defs
-        self.inputs = set()
-        self.scoped = set()
-
-    def visitProg(self, ctx: RParser.ProgContext):
-        self.visitChildren(ctx)
-        # remove all inputs with conf_ or param_ prefix
-        self.inputs = {i for i in self.inputs if not i.startswith("conf_") and not i.startswith("param_")}
-        # remove all built in constants ["T", "F", "pi", "is.numeric", "mu", "round"]
-        self.inputs = {i for i in self.inputs if i not in ["T", "F", "pi", "is.numeric", "mu", "round"]}
-        return self.inputs
-
-    def visitSublist(self, ctx: RParser.SublistContext):
-        self.visitChildren(ctx)
-
-    def visitSub(self, ctx: RParser.SubContext):
-        # If function, add all its arguments to scoped, which we find using regex
-        if ctx.getText().startswith("function"):
-            ids = re.findall(r'\((.*?)\)', ctx.getText())[0].split(", ")
-            ids = ids[0].split(",")
-
-            # add all ids to scoped
-            for id in ids:
-                self.scoped.add(id)
-
-            self.visitChildren(ctx)
-            # remove all ids from scoped
-            for id in ids:
-                self.scoped.remove(id)
-            return None
-
-        if ctx.expr() is not None:
-            self.visitChildren(ctx)
-
-    def visitFor(self, ctx: RParser.ForContext):
-        # Iterator variable is scoped
-        self.scoped.add(ctx.ID().getText())
-        self.visit(ctx.expr(0))
-        self.visit(ctx.expr(1))
-        self.scoped.remove(ctx.ID().getText())
-        return None
-
-    def visitAssign(self, ctx: RParser.AssignContext):
-        return None
-
-    def visitCall(self, ctx: RParser.CallContext):
-        # Skip import calls
-        if ctx.expr().getText() == "library" or ctx.expr().getText() == "require":
-            return None
-
-        self.visit(ctx.sublist())
-
-    def visitId(self, ctx: RParser.IdContext):
-        # Only add IDs to input if they have not been defined and are not scoped.
-        if ctx.getText() not in self.defs and ctx.getText() not in self.scoped:
-            self.inputs.add(ctx.getText())
-        return None
 
 
 class ExtractParams(RVisitor):
@@ -163,74 +253,6 @@ class ExtractConfigs(RVisitor):
         return ctx.STRING().getText()
 
 
-class ExtractNames(RVisitor):
-    # Build a dictionairy to keep track of all identifiers and their data types
-    def __init__(self):
-        self.names = {}
-
-    def visitProg(self, ctx: RParser.ProgContext):
-        self.visitChildren(ctx)
-        return self.names
-
-    def visitAssign(self, ctx: RParser.AssignContext):
-        # Get the identifier and the assigned value of the expr and add to dict.
-        id = self.visit(ctx.expr(0))
-        self.names[id] = self.visit(ctx.expr(1))
-        return None
-
-    def visitAddsub(self, ctx: RParser.AddsubContext):
-        # Visit left and right expressions
-        left = self.visit(ctx.expr(0))
-        right = self.visit(ctx.expr(1))
-
-        # Check if left and right are in dict i.e. they're variables
-        if left in self.names:
-            left = self.names[left]
-        if right in self.names:
-            right = self.names[right]
-
-        # Check if the left and right children are of the same type
-        if (left == "int" and right == "int"):
-            return "int"
-        elif (left == "float" or right == "float"):
-            return "float"
-        else:
-            return None
-
-    def visitMuldiv(self, ctx: RParser.MuldivContext):
-        # Same procedure as addsub
-        left = self.visit(ctx.expr(0))
-        right = self.visit(ctx.expr(1))
-
-        if left in self.names:
-            left = self.names[left]
-        if right in self.names:
-            right = self.names[right]
-
-        if (left == "int" and right == "int"):
-            return "int"
-        elif (left == "float" or right == "float"):
-            return "float"
-        else:
-            return None
-
-    def visitId(self, ctx: RParser.IdContext):
-        id = ctx.ID().getText()
-        # Check if id is in dict, otherwise add it.
-        if id not in self.names:
-            self.names[id] = None
-        return id
-
-    def visitInt(self, ctx: RParser.IntContext):
-        return "int"
-
-    def visitFloat(self, ctx: RParser.FloatContext):
-        return "float"
-
-    def visitString(self, ctx: RParser.StringContext):
-        return "string"
-
-
 class ExtractImports(RVisitor):
     def __init__(self):
         self.imports = {}
@@ -252,3 +274,63 @@ class ExtractImports(RVisitor):
 
     def visitString(self, ctx: RParser.StringContext):
         return ctx.STRING().getText()
+
+    # class ExtractInputs(RVisitor):
+    #     def __init__(self, defs):
+    #         self.defs = defs
+    #         self.inputs = set()
+    #         self.scoped = set()
+    #
+    #     def visitProg(self, ctx: RParser.ProgContext):
+    #         self.visitChildren(ctx)
+    #         # remove all inputs with conf_ or param_ prefix
+    #         self.inputs = {i for i in self.inputs if not i.startswith("conf_") and not i.startswith("param_")}
+    #         # remove all built in constants ["T", "F", "pi", "is.numeric", "mu", "round"]
+    #         self.inputs = {i for i in self.inputs if i not in ["T", "F", "pi", "is.numeric", "mu", "round"]}
+    #         return self.inputs
+    #
+    #     def visitSublist(self, ctx: RParser.SublistContext):
+    #         self.visitChildren(ctx)
+    #
+    #     def visitSub(self, ctx: RParser.SubContext):
+    #         # If function, add all its arguments to scoped, which we find using regex
+    #         if ctx.getText().startswith("function"):
+    #             ids = re.findall(r'\((.*?)\)', ctx.getText())[0].split(", ")
+    #             ids = ids[0].split(",")
+    #
+    #             # add all ids to scoped
+    #             for id in ids:
+    #                 self.scoped.add(id)
+    #
+    #             self.visitChildren(ctx)
+    #             # remove all ids from scoped
+    #             for id in ids:
+    #                 self.scoped.remove(id)
+    #             return None
+    #
+    #         if ctx.expr() is not None:
+    #             self.visitChildren(ctx)
+    #
+    #     def visitFor(self, ctx: RParser.ForContext):
+    #         # Iterator variable is scoped
+    #         self.scoped.add(ctx.ID().getText())
+    #         self.visit(ctx.expr(0))
+    #         self.visit(ctx.expr(1))
+    #         self.scoped.remove(ctx.ID().getText())
+    #         return None
+    #
+    #     def visitAssign(self, ctx: RParser.AssignContext):
+    #         return None
+    #
+    #     def visitCall(self, ctx: RParser.CallContext):
+    #         # Skip import calls
+    #         if ctx.expr().getText() == "library" or ctx.expr().getText() == "require":
+    #             return None
+    #
+    #         self.visit(ctx.sublist())
+    #
+    #     def visitId(self, ctx: RParser.IdContext):
+    #         # Only add IDs to input if they have not been defined and are not scoped.
+    #         if ctx.getText() not in self.defs and ctx.getText() not in self.scoped:
+    #             self.inputs.add(ctx.getText())
+    #         return None
