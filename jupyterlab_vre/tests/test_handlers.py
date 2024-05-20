bb83679effe84dbbe725b7dd83dc993457877cfb
skoulouzis <koulouzis@gmail.com>
2024-05-21 00:06:05 +0200

    fire cell creation and then wait for completion

--- jupyterlab_vre/tests/test_handlers.py
+++ jupyterlab_vre/tests/test_handlers.py
@@ -260,15 +260,18 @@ class HandlersAPITest(AsyncHTTPTestCase):
             workflow_file_path = os.path.join(workflow_path, workflow_file)
             with open(workflow_file_path, 'r') as read_file:
                 payload = json.load(read_file)
-            cells = payload['nodes']
-            for cell in cells:
-                cell_title = cell['properties']['title']
+            cells = payload['chart']['nodes']
+            cell_paths = set()
+            for cell_id in cells:
+                cell_title = cells[cell_id]['properties']['title']
                 for cell_file in cells_files:
-                    cell_path = os.path.join(cells_json_path, cell_file)
+                    cell_path  =os.path.join(cells_json_path, cell_file)
                     with open(cell_path, 'r') as read_file:
                         cell = json.load(read_file)
                     if cell['title'] == cell_title:
-                        self.add_cell_to_cat(cell_path=cell_path)
+                        cell_paths.add(cell_path)
+            self.add_cells_to_cat(cell_paths=cell_paths)
+
             response = self.fetch('/executeworkflowhandler', method='POST', body=json.dumps(payload))
             self.assertEqual(response.code, 200, response.body)
             json_response = json.loads(response.body.decode('utf-8'))
@@ -361,26 +364,32 @@ class HandlersAPITest(AsyncHTTPTestCase):
         else:
             del os.environ["DEBUG"]
 
-    def add_cell_to_cat(self, cell_path=None):
-        create_cell_and_add_to_cat(cell_path=cell_path)
-        response = self.call_cell_handler()
-        wf_creation_utc = datetime.datetime.now(tz=datetime.timezone.utc)
-        self.assertEqual(200, response.code)
-        git_wf_id = json.loads(response.body.decode('utf-8'))['wf_id']
+    def add_cells_to_cat(self, cell_paths=None):
+        os.environ["DEBUG"] = "True"
+        wf_ids_and_creation_utc = []
+        for cell_path in cell_paths:
+            create_cell_and_add_to_cat(cell_path=cell_path)
+            response = self.call_cell_handler()
+            entry = {'wf_creation_utc': datetime.datetime.now(tz=datetime.timezone.utc)}
+            self.assertEqual(200, response.code)
+            entry['wf_id'] = json.loads(response.body.decode('utf-8'))['wf_id']
+            wf_ids_and_creation_utc.append(entry)
         cat_repositories = Catalog.get_repositories()
         repo = cat_repositories[0]
         repo_token = repo['token']
 
         owner, repository_name = repo['url'].removeprefix('https://github.com/').split('/')
-        job = wait_for_job(
-            wf_id=git_wf_id,
-            wf_creation_utc=wf_creation_utc,
-            owner=owner,
-            repository_name=repository_name,
-            token=repo_token,
-            job_id=None,
-            timeout=300,
-            wait_for_completion=True,
-        )
-        self.assertIsNotNone(job, 'Job not found')
-        self.assertEqual('completed', job['status'], 'Job not completed')
+        for entry in wf_ids_and_creation_utc:
+            job = wait_for_job(
+                wf_id=entry['wf_id'],
+                wf_creation_utc=entry['wf_creation_utc'],
+                owner=owner,
+                repository_name=repository_name,
+                token=repo_token,
+                job_id=None,
+                timeout=300,
+                wait_for_completion=True,
+            )
+            self.assertIsNotNone(job, 'Job not found')
+            self.assertEqual('completed', job['status'], 'Job not completed')
+
